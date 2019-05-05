5c131d54e633c5ddc9a45b094434a342eda53038
Mrp4m <pmbyn.ofc@gmail.com>
2019-05-06 00:46:24 +0700

    bla bla bla

---
+++ OvertimeProject/scripts/js/Manager.js
@@ -0,0 +1,41 @@
+﻿function reject(id) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, reject it!'
+    }).then((result) => {
+        if (result.value) {
+            $('#responsive-modal').modal('hide');
+            Swal.fire(
+              'Rejected!',
+              'Your file has been rejected.',
+              'success'
+            )
+        }
+    })
+}
+
+function accept(id) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, accept it!'
+    }).then((result) => {
+        if (result.value) {
+            $('#responsive-modal').modal('hide');
+            Swal.fire(
+              'Accepted!',
+              'Your file has been accepted.',
+              'success'
+            )
+        }
+    })
+}
